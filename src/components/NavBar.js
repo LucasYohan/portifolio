0aa858260282493a721add79a5842ce86213d9c0
Lucas Yohan <lucasyohanfmarinho@gmail.com>
2025-01-23 19:29:10 -0300

    Atualizacao navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,18 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import './NavBar.css'
+import './NavBar.css';
 
 const NavBar = () => {
-    return (
-        <div className='NavBar'>
+    const [showDropdown, setShowDropdown] = useState(false);
 
-            <h2>
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    return (
+        <div className="NavBar">
+            <h2 className='titulo'>
                 <Link to={"/"}>Lucas Yohan</Link>
             </h2>
 
@@ -15,17 +21,25 @@ const NavBar = () => {
                     <Link to={"/"}>Home</Link>
                 </li>
 
-                <li>
-                    <Link to={"/projetos"}>Projetos</Link>
+                <li className="dropdown">
+                    <button onClick={toggleDropdown} className="dropdown-button">
+                        Projetos
+                    </button>
+                    <ul className={`dropdown-menu ${showDropdown ? 'show' : ''}`}>
+                        <li><Link to={"/projeto1"}>Teste 1</Link></li>
+                        <li><Link to={"/projeto2"}>Teste 2</Link></li>
+                        <li><Link to={"/projeto3"}>Teste 3</Link></li>
+                        <li><Link to={"/projeto4"}>Teste 4</Link></li>
+                        <li><Link to={"/projeto5"}>Teste 5</Link></li>
+                    </ul>
                 </li>
 
                 <li>
                     <Link to={"/contatos"}>Contatos</Link>
                 </li>
-
             </ul>
         </div>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
