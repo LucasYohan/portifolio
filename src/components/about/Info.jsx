d2ebdd23b266ee11ce34d473e2e683baf9594d88
Matheus França <lucasyohanfmarinho@gmail.com>
2025-09-18 19:21:30 -0300

    tela sobre mim completa

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,6 +1,25 @@
-
 function Info() {
-  return <div>Info</div>;
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="uil uil-backpack about__icon"></i>
+        <h3 className="about__tittle">Experiência</h3>
+        <span className="about__subtittle">1 ano e meio</span>
+      </div>
+
+      <div className="about__box">
+        <i class="uil uil-award-alt about__icon"></i>
+        <h3 className="about__tittle">Formações</h3>
+        <span className="about__subtittle">Técnico - ADS</span>
+      </div>
+
+      <div className="about__box">
+        <i class="uil uil-hard-hat about__icon"></i>
+        <h3 className="about__tittle">Trabalhando</h3>
+        <span className="about__subtittle">Não, disponivel de imediato</span>
+      </div>
+    </div>
+  );
 }
 
 export default Info;
