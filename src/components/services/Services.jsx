be94f7e0b7940781b7496b4b1487f4fed1f77957
Roberta França <lucasyohanfmarinho@gmail.com>
2025-09-21 22:03:36 -0300

    correcao

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -130,7 +130,9 @@ function Services() {
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Organização e foco</p>
+                  <p className="services__modal-info">
+                    Organização e disciplina
+                  </p>
                 </li>
               </ul>
             </div>
